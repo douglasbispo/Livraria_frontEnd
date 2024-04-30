9dfc0b1818093021aa5977db723786af7a1d9ebf
Douglas Santos <dsanttos363@gmail.com>
2024-04-30 00:54:32 -0300

    First commit

---
+++ projeto/assets/js/owl/setup.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop: false,
+    margin: 0,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        320: {
+            items: 2
+        },
+        530: {
+            items: 3
+        },
+        800: {
+            items: 4
+        },
+        1100: {
+            items: 5
+        }
+    }
+})
